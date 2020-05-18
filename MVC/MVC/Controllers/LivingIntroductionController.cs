171d90fe3d1d1e5dab9e0f4034bfcb26e570382d
wen <waynevirus1983@gmail.com>
2020-05-18 17:06:08 +0800

    試試看

---
+++ MVC/MVC/Controllers/LivingIntroductionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class LivingIntroductionController : Controller
+    {
+        // GET: LivingIntroduction
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC/MVC/Controllers/MvcHomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class MvcHomeController : Controller
+    {
+        // GET: MvcHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
