5a403077bd607da9ae2749041977ffb43ed867e3
wen <waynevirus1983@gmail.com>
2020-05-20 13:25:23 +0800

    購物車

---
+++ MVC/MVC/Assest/js/_NavbarPartial.js
@@ -0,0 +1,18 @@
+﻿function shop() {
+    var check = document.getElementById("check");
+    var body = document.getElementById("body");
+
+
+    var value = check.checked;
+
+    if (check.checked == true) {
+        // body.style.opacity = '0.6';
+        //   shop.style.opacity = '1';
+        check.checked = false;
+    }
+    else {
+        // var shop = document.querySelector("#shop"); 
+
+        check.checked = true;
+    }
+}
