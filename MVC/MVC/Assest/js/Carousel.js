15e4796004d5e2d491b3cbb6f6f1d5a8764b64fb
wen <waynevirus1983@gmail.com>
2020-05-22 15:04:45 +0800

    輪播

---
+++ MVC/MVC/Assest/js/Carousel.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+    $('.multiple-items').slick({
+        centerMode: true,
+        centerPadding: '0px',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrow: true,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        dots: false,
+        centerMode: true,
+    });
+})
