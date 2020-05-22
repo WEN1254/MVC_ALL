08cf8cfa94d28054c798ce47bb0f492cb8af8029
wen <waynevirus1983@gmail.com>
2020-05-22 17:21:47 +0800

    篩選和版面的效果

---
+++ MVC/MVC/Assest/js/ProductList.js
@@ -0,0 +1,17 @@
+﻿function match() {
+    var check = document.getElementById("check_order");
+
+
+
+    var value = check.checked;
+
+    if (check.checked == true) {
+
+        check.checked = false;
+    }
+    else {
+
+
+        check.checked = true;
+    }
+}
